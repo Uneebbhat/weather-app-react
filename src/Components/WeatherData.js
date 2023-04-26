803b323aa74b4959522da225a662913bc32e3d1e
Uneeb <uneebbhatti3@gmail.com>
2023-04-26 20:33:34 +0500

    added new data

--- src/Components/WeatherData.js
+++ src/Components/WeatherData.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function WeatherData() {
   const apiKey = "87351461c2303b55b8f703aedb4e7b4a";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=87351461c2303b55b8f703aedb4e7b4a&units=metric`;
 
   const [weatherData, setWeatherData] = useState(null);
 
@@ -14,11 +14,18 @@ export default function WeatherData() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>React Weather App</h1>
       {weatherData && (
         <div>
           <p>Wind Speed: {weatherData.wind.speed}</p>
+          <p>Weather Condition: {weatherData.weather[0].main}</p>
           <p>Wind Direction: {weatherData.wind.deg}</p>
+          <p>Wind Direction: {weatherData.main.temp}</p>
+          <p>Wind Direction: {weatherData.main.feels_like}</p>
+          <p>Wind Direction: {weatherData.main.humidity}</p>
+          <p>
+            Country: {weatherData.sys.country}, {weatherData.name}
+          </p>
         </div>
       )}
     </>
